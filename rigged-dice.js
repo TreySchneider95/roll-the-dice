b37f67bce9c5d21c448c7f549dc8e88a079a72f0
TreySchneider95 <tschneid95@gmail.com>
2022-09-20 13:00:28 -0600

    added object counter

--- rigged-dice.js
+++ rigged-dice.js
@@ -1,10 +1,12 @@
 const prompt = require('prompt-sync')({sigint: true});
-
-
+let statObj = {1:0,2:0,3:0,4:0,5:0,6:0}
 let rigNum = Number(prompt("Enter number to have rigged: "))
-let die = console.log(Math.ceil(Math.random()*7))
-if(die === 7){
-    console.log(`rigged ${rigNum}`)
-}else{
-    console.log(die)
+for(x of Array(100000000)){
+    let die = Math.ceil(Math.random()*7)
+    if(die === 7){
+        statObj[Number(rigNum)] += 1
+    }else{
+        statObj[Number(die)] += 1
+    }
 }
+console.log(statObj)
