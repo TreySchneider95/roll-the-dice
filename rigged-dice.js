2bfa7ac91e15e978db7a78b7f898774d462c9819
TreySchneider95 <tschneid95@gmail.com>
2022-09-20 13:01:50 -0600

    changed array length to something more reasonable

--- rigged-dice.js
+++ rigged-dice.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')({sigint: true});
 let statObj = {1:0,2:0,3:0,4:0,5:0,6:0}
 let rigNum = Number(prompt("Enter number to have rigged: "))
-for(x of Array(100000000)){
+for(x of Array(1000)){
     let die = Math.ceil(Math.random()*7)
     if(die === 7){
         statObj[Number(rigNum)] += 1
